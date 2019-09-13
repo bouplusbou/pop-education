6c5d331c2c662259ed42e3f1d3a327564e5724bd
Baptiste Boucher <baptiste.boucher@gmail.com>
2019-09-13 21:04:02 +0200

    first push

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,13 @@
 import React from "react"
-import { Link } from "gatsby"
-
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <SEO title="Pop Education - En construction" />
+    <h1>Bonjour !</h1>
+    <p>Le site est actuellement en construction.</p>
+    <p>Vous pouvez contacter Marie via cette adresse: marie@pop-education.fr</p>
   </Layout>
 )
 
